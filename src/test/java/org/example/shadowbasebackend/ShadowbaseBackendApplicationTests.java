bc90c1c8e84cb89e68ff6fc2b3a37f4c1fbdd7ca
gayatrideo06-design <gayatrideo06@gmail.com>
2026-07-20 21:36:33 +0530

    Added ShadowbaseBackendApplication

---
+++ src/test/java/org/example/shadowbasebackend/ShadowbaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shadowbasebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShadowbaseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
